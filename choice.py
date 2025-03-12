387d9eaa50c733d0d0c7a2cb9620c8e533bbd798
BatMatt71 <84284007+BatMatt71@users.noreply.github.com>
2025-03-12 10:58:32 +0000

    Delete choice.py

--- choice.py
+++
@@ -1,6 +0,0 @@
-import options
-
-def MathOption(nums, option):
-    optionList = [options.Exit, options.Add, options.Sub, options.Mult, options.Divid, options.Modul, options.Power, options.SqrRoot]
-
-    return optionList[option](nums)
