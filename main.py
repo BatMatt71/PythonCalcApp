b661bc0980601e7b5249b3ca4ab18451cd705e9f
BatMatt71 <84284007+BatMatt71@users.noreply.github.com>
2025-03-12 10:59:03 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#calculator.py
+
+#Introduction: completing a program for assignment n2
+import inputData
+
+def main():
+    print("|| Welcome to the Calculator App ||\n")
+    
+    #List that holds both numbers
+    resNum = inputData()
+
+    print("Answer:", resNum)
+
+
+#Code initialiser
+if __name__ == "__main__":
+    main()
