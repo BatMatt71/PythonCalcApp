dab9cd0338da88e97a789322593d8e7f9a491148
BatMatt71 <84284007+BatMatt71@users.noreply.github.com>
2025-04-08 18:57:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# Author:
-# Date: 
-# Program Name: 
-# Purpose:
+# Author: George Matei Macineanu
+# Date: 19/03/2025
+# Program Name: main.py
+# Purpose: bringing together the other files together and outputting results
 # Assignment No2 Programming principles
 
 #main.py
